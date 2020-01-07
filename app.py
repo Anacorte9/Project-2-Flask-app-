c2d3165fa936400147f20267b4b2e7ad7fa06b36
Anacorte9 <cdaguerreup@gmail.com>
2020-01-06 18:34:57 -0600

    adding data

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-# app.py
+
+import sqlite3
+import pandas as pd
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
@@ -8,6 +10,13 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
+@app.route('/API/data')
+def get_data():
+    w2 = pd.read_sql_query("SELECT * FROM hawaii" , cnx)
+    res = w2.to_json(orient='table')
+    return jsonify(res)
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
+
